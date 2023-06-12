f800bd0519a24ccac5837de4d2e43d307a341b2f
SinanSchlagi <sinanschlageter@gmx.de>
2023-06-12 16:48:50 +0200

    Task 1 done

--- src/idh/java/Tree.java
+++ src/idh/java/Tree.java
@@ -4,21 +4,20 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Tree<T>  {
-
+public class Tree<T> {
 	T value;
 	Set<Tree<T>> children;
-	
+
 	public Tree(T value) {
 		this.value = value;
 		this.children = new HashSet<Tree<T>>();
 	}
-	
+
 	public Tree(T value, Collection<Tree<T>> children) {
 		this.value = value;
 		this.children = new HashSet<Tree<T>>(children);
 	}
-		
+
 	public T get() {
 		return value;
 	}
@@ -30,18 +29,20 @@ public class Tree<T>  {
 	public Set<Tree<T>> children() {
 		return children;
 	}
-	
-	
-	public void dfs() {
+
+	public void dfs(int i) {
+
+		for (int j = 0; j < i; j++) {
+			System.out.print("	");
+		}
 		System.out.println(this.value);
 		for (Tree<T> child : children) {
-			child.dfs();
+			child.dfs(i + 1);
 		}
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Tree<String> ebike = new Tree<String>("e-bike");
 		Tree<String> tandem = new Tree<String>("tandem");
 		Tree<String> bike = new Tree<String>("bike");
@@ -52,8 +53,8 @@ public class Tree<T>  {
 		wheeled_vehicle.children().add(buggy);
 		bike.children().add(tandem);
 		bike.children().add(ebike);
-		
-		wheeled_vehicle.dfs();
+
+		wheeled_vehicle.dfs(0);
 	}
 
 }
