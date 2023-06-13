954ef66287c2d7396312e24205f30da26d4487b7
TabeaStoerbrock <stoerbrock.tabea@gmail.com>
2023-06-13 22:28:23 +0200

    Aufgabe 8
    Tree anzeigen mit tabs (dfs)
    Hanoi spielen für Faule 8-) (auto-mode)

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -7,32 +7,43 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Hanoi {
-
 	Deque<Integer> left = new LinkedList<Integer>();
 	Deque<Integer> middle = new LinkedList<Integer>();
 	Deque<Integer> right = new LinkedList<Integer>();
-	
+
 	public Hanoi() {
-		for (int i = 3; i < 10; i++) {
+		for (int i = 3; i < 6; i++) {
 			left.addLast(i);
 		}
 	}
-	
+
 	private void movePiece(char from, char to) {
 		Integer piece = null;
-		switch(from) {
-		case 'l': piece = left.remove(); break;
-		case 'm': piece = middle.remove(); break;
-		case 'r': piece = right.remove(); break;
+		switch (from) {
+		case 'l':
+			piece = left.remove();
+			break;
+		case 'm':
+			piece = middle.remove();
+			break;
+		case 'r':
+			piece = right.remove();
+			break;
 		}
-		
-		switch(to) {
-		case 'l': left.addFirst(piece); break;
-		case 'm': middle.addFirst(piece); break;
-		case 'r': right.addFirst(piece); break;
+
+		switch (to) {
+		case 'l':
+			left.addFirst(piece);
+			break;
+		case 'm':
+			middle.addFirst(piece);
+			break;
+		case 'r':
+			right.addFirst(piece);
+			break;
 		}
 	}
-	
+
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -48,47 +59,49 @@ public class Hanoi {
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				e.printStackTrace();
-			} 
+			}
 		}
 	}
-	
+
 	private Iterator<Integer> getLeftDescendingIterator() {
 		return left.descendingIterator();
 
 	}
+
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		return middle.descendingIterator();
 
 	}
+
 	private Iterator<Integer> getRightDescendingIterator() {
 		return right.descendingIterator();
 	}
-	
+
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
 		iter = this.getLeftDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n m|");
 		iter = this.getMiddleDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |\n r|");
 		iter = this.getRightDescendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
 		b.append("\n  |");
 		return b.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		// hanoi.run();
@@ -101,15 +114,74 @@ public class Hanoi {
 
 		// this is the entry point into the recursion
 		movePieces(left.size(), 'l', 'r', 'm');
-		
+
 		// and in the end, we check that everything has been moved
 		System.out.println(this);
 	}
-	
+
+	// l -m - r
 	private void movePieces(int numberOfPieces, char from, char to, char util) {
-		// TODO: Implement me!
+		// wenn Spielsteine nicht mehr benutzt werden = 0
+
+		System.out.println(this);
+		if (numberOfPieces == 0) {
+			return;
+		}
+		// Stapelgröße = 2
+		
+		//Übersicht gewinnen: Was ist hier los!
+//		System.out.println(numberOfPieces);
+//		System.out.println(from);
+//		System.out.println(to);
+//		System.out.println(util);
+
+		// l, r, m
+		// was ist der höchste Stapel? 
+		
+		
+		
+		//alle außer unterste verschieben auf util
+		movePieces(numberOfPieces - 1, from, util, to);
+		//auf Zielstapel schieben
+		movePiece(from, to);
+		//Übersicht
+		System.out.println("x");
+		movePieces(numberOfPieces - 1, util, to, from);
+		
+		
+//Hilfe durch spielen des Spiels ->
+//Schritte, die durchgeführt werden sollen im "echten" Spiel:
+		//		from = l
+		//		to = right
+		//		util = m 
+		//		
+		//		2. Schritt from = l
+		//		to =m
+		//		util = r
+		//		
+		//		3. Schritt
+		//		from = r
+		//		to =m
+		//		util = l
+		//		
+		//		4. Schritt
+		//		from = l
+		//		to = r
+		//		util = m
+		//		
+		//		5. Schritt
+		//		from = m
+		//		to = l
+		//		util = r
+		//		
+		//		6. from = m
+		//		to = r
+		//		util = l
+		//		
+		//		7 from = l
+		//		to = r 
+		//		util = m
+
 	}
-	
-	
 
 }
