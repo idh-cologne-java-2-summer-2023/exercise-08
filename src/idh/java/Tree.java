67a9716931c1601f512b8e6bb3f0a797bf23e6ce
Me9Rs <MR1303@web.de>
2023-06-13 13:26:52 +0200

    ADDED: Exercise 1 (Branch indent)

--- src/idh/java/Tree.java
+++ src/idh/java/Tree.java
@@ -4,21 +4,21 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Tree<T>  {
+public class Tree<T> {
 
 	T value;
 	Set<Tree<T>> children;
-	
+
 	public Tree(T value) {
 		this.value = value;
 		this.children = new HashSet<Tree<T>>();
 	}
-	
+
 	public Tree(T value, Collection<Tree<T>> children) {
 		this.value = value;
 		this.children = new HashSet<Tree<T>>(children);
 	}
-		
+
 	public T get() {
 		return value;
 	}
@@ -30,18 +30,21 @@ public class Tree<T>  {
 	public Set<Tree<T>> children() {
 		return children;
 	}
-	
-	
-	public void dfs() {
-		System.out.println(this.value);
+
+	public void dfs(int branchNumber) {
+		String branchIndent = "";
+		for (int i = 0; i < branchNumber; i++) {
+			branchIndent += "  ";
+		}
+
+		System.out.println(branchIndent + this.value);
 		for (Tree<T> child : children) {
-			child.dfs();
+			child.dfs(branchNumber + 1);
 		}
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Tree<String> ebike = new Tree<String>("e-bike");
 		Tree<String> tandem = new Tree<String>("tandem");
 		Tree<String> bike = new Tree<String>("bike");
@@ -52,8 +55,8 @@ public class Tree<T>  {
 		wheeled_vehicle.children().add(buggy);
 		bike.children().add(tandem);
 		bike.children().add(ebike);
-		
-		wheeled_vehicle.dfs();
+
+		wheeled_vehicle.dfs(0);
 	}
 
 }
