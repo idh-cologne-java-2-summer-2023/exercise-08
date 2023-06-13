429690fd84c7caa2f19abcb9c85274b2ff96c575
NilsKEA <nallstae@smail.uni-koeln.de>
2023-06-13 18:17:16 +0200

    tree pt.1

--- src/idh/java/Tree.java
+++ src/idh/java/Tree.java
@@ -1,59 +1,59 @@
 package idh.java;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Tree<T>  {
+public class Tree<T> {
+    private T data; // Data stored in the node
+    private List<Tree<T>> children; // List of child nodes
 
-	T value;
-	Set<Tree<T>> children;
-	
-	public Tree(T value) {
-		this.value = value;
-		this.children = new HashSet<Tree<T>>();
-	}
-	
-	public Tree(T value, Collection<Tree<T>> children) {
-		this.value = value;
-		this.children = new HashSet<Tree<T>>(children);
-	}
-		
-	public T get() {
-		return value;
-	}
+    // Constructor to initialize the node with data
+    public Tree(T data) {
+        this.data = data;
+        this.children = new ArrayList<>();
+    }
 
-	public void set(T value) {
-		this.value = value;
-	}
+    // Getter for the data
+    public T getData() {
+        return data;
+    }
 
-	public Set<Tree<T>> children() {
-		return children;
-	}
-	
-	
-	public void dfs() {
-		System.out.println(this.value);
-		for (Tree<T> child : children) {
-			child.dfs();
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		Tree<String> ebike = new Tree<String>("e-bike");
-		Tree<String> tandem = new Tree<String>("tandem");
-		Tree<String> bike = new Tree<String>("bike");
-		Tree<String> buggy = new Tree<String>("buggy");
-		Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
+    // Method to add a child node to the current node
+    public List<Tree<T>> children() {
+        return children;
+    }
 
-		wheeled_vehicle.children().add(bike);
-		wheeled_vehicle.children().add(buggy);
-		bike.children().add(tandem);
-		bike.children().add(ebike);
-		
-		wheeled_vehicle.dfs();
-	}
+    // Depth-first search (DFS) traversal of the tree with indentation
+ // Depth-first search (DFS) traversal of the tree with indentation
+    public void dfs(int depth) {
+        // Create the indentation string based on the current depth
+        StringBuilder indentation = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            indentation.append("  "); // Add two spaces for each level of depth
+        }
 
+        // Print the indented data of the current node
+        System.out.println(indentation.toString() + data);
+
+        // Recursively traverse and print the children nodes with increased depth
+        for (Tree<T> child : children) {
+            child.dfs(depth + 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Tree<String> ebike = new Tree<String>("e-bike");
+        Tree<String> tandem = new Tree<String>("tandem");
+        Tree<String> bike = new Tree<String>("bike");
+        Tree<String> buggy = new Tree<String>("buggy");
+        Tree<String> wheeled_vehicle = new Tree<String>("wheeled vehicle");
+
+        wheeled_vehicle.children().add(bike);
+        wheeled_vehicle.children().add(buggy);
+        bike.children().add(tandem);
+        bike.children().add(ebike);
+
+        wheeled_vehicle.dfs(0);
+    }
 }
